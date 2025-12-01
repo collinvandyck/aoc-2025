8dd453b1d28bab7119862b26552e0f867fa89446
Collin Van Dyck <collin@5xx.engineer>
2025-12-01 18:57:08 -0500

    wip

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,8 +1,8 @@
 use std::iter;
 
 fn main() {
-    let ex1 = aoc::load_data("01/ex1").unwrap();
-    let in1 = aoc::load_data("01/in1").unwrap();
+    let ex1 = aoc::load_data("01/ex1");
+    let in1 = aoc::load_data("01/in1");
     println!("ex1: {}", run(&ex1, true));
     println!("pt1: {}", run(&in1, true));
     println!("ex2: {}", run(&ex1, false));
@@ -10,24 +10,41 @@ fn main() {
 }
 
 fn run(data: &str, pt1: bool) -> usize {
-    data.lines()
-        .map(|s| {
-            let dir = &s[0..1];
-            let mut amt = (&s[1..]).parse::<i64>().unwrap();
-            if dir == "L" {
-                amt *= -1
-            }
-            amt
-        })
-        .fold((0, 50), |(mut res, mut cur), amt| {
-            if pt1 {
-                cur = (((cur + amt) % 100) + 100) % 100;
-                res += (cur == 0).then_some(1).unwrap_or_default();
-            } else {
-                let delta = cur + amt;
-                cur = (((cur + amt) % 100) + 100) % 100;
-            }
-            (res, cur)
-        })
-        .0
+    let mut cur = 50;
+    let mut res = 0;
+    let deltas = data.lines().map(|s| {
+        let dir = &s[0..1];
+        let mut amt = (&s[1..]).parse::<i64>().unwrap();
+        if dir == "L" {
+            amt *= -1
+        }
+        amt
+    });
+    for amt in deltas {
+        cur = (((cur + amt) % 100) + 100) % 100;
+        res += (cur == 0).then_some(1).unwrap_or_default();
+    }
+    return res;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_spans() {
+        let cur = 50;
+        let amt = -60;
+        // i want to verify that we can calculate the number of clicks
+        // that include 0 are correct.
+        let dlt = cur + amt;
+        assert_eq!(dlt, -10);
+        // so now we have --10 -> 50
+        let r = std::ops::Range {
+            start: dlt,
+            end: cur,
+        };
+
+        // verify that our modulo arithmetic is ok
+        let cur = (((cur + amt) % 100) + 100) % 100;
+        assert_eq!(cur, 90)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use anyhow::{Context, Result};
 
-pub fn load_data(day: &str) -> Result<String> {
+pub fn load_data(day: &str) -> String {
     std::fs::read(&format!("data/{day}"))
         .context("could not read data")
         .and_then(|bs| String::from_utf8(bs).context("convert bytes to utf8"))
         .map(|s| s.trim().to_string())
+        .unwrap()
 }
