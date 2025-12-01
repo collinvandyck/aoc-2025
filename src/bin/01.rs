8193387fa18138c581c0abb16daf99672dc11ecb
Collin Van Dyck <collin@5xx.engineer>
2025-12-01 07:33:41 -0500

    wip

---
+++ src/bin/01.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let ex1 = aoc::load_data("01/ex1").unwrap();
+    println!("{ex1}");
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use anyhow::{Context, Result};
+
+pub fn load_data(day: &str) -> Result<String> {
+    std::fs::read(&format!("data/{day}"))
+        .context("could not read data")
+        .and_then(|bs| String::from_utf8(bs).context("convert bytes to utf8"))
+        .map(|s| s.trim().to_string())
+}
