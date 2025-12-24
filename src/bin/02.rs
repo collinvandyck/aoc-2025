e72546ff3f54c93b8841c486e764b55bba0b49cf
Collin Van Dyck <collin@5xx.engineer>
2025-12-24 13:12:49 -0500

    day 2 done

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 static EX1: &str = include_str!("../../data/02/ex1");
 static IN1: &str = include_str!("../../data/02/in1");
 
@@ -5,7 +7,7 @@ fn main() {
     println!("ex1: {}", run(EX1, true));
     println!("pt1: {}", run(IN1, true));
     println!("ex2: {}", run(EX1, false));
-    //println!("pt2: {}", run(IN1, false));
+    println!("pt2: {}", run(IN1, false));
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -17,40 +19,25 @@ struct Range {
 fn run(data: &str, pt1: bool) -> usize {
     parse(data)
         .into_iter()
-        .flat_map(|r| (r.from..=r.to))
-        .filter(|v| (pt1 && repeats_twice(v)) || (!pt1 && repeats_any(v)))
+        .flat_map(|r| r.from..=r.to)
+        .filter(|v| (pt1 && repeats_twice(v)) || (!pt1 && repeats_any(*v)))
         .sum()
 }
 
-fn repeats_any(&v: &usize) -> bool {
-    if v != 1012 {
+fn repeats_any(v: usize) -> bool {
+    if v < 10 {
         return false;
     }
-
-    let digits = num_digits(v); // eg: 8
-    println!("repeats any: {v} digits: {digits}");
-    for run_len in 1..=(digits / 2) {
-        if digits % run_len != 0 {
-            continue;
-        }
-        let pow = 10_usize.pow(digits as u32);
-        let pow_slice = 10_usize.pow(run_len as u32);
-        println!("> v: {v} run len {run_len} pow_slice: {pow_slice}");
-        for idx in (0..(digits / run_len)) {
-            let div_amt =
-                10_usize.pow(((digits / run_len - idx - 1) * run_len) as u32);
-            let mod_amt = pow_slice.pow((idx) as u32);
-            let div = v / div_amt;
-            println!(
-                "> idx: {idx} run_len: {run_len} div_amt: {div_amt} div: {div} foo:{mod_amt}"
-            );
-        }
-    }
-    false
+    let num_digits = num_digits(v);
+    (1..=(num_digits / 2)).filter(|x| num_digits % x == 0).any(|run_len| {
+        (0..(num_digits / run_len))
+            .map(|part| (v / tenpow(part * run_len)) % tenpow(run_len))
+            .all_equal()
+    })
 }
 
-fn slices_of(v: usize, run_len: usize) -> Vec<usize> {
-    vec![]
+fn tenpow(amt: usize) -> usize {
+    10_usize.pow(amt as u32)
 }
 
 fn repeats_twice(&v: &usize) -> bool {
@@ -61,10 +48,6 @@ fn repeats_twice(&v: &usize) -> bool {
     let mid_pow10 = 10_usize.pow(digits as u32 / 2);
     let (lhs, rhs) = (v / mid_pow10, v % mid_pow10);
     lhs == rhs
-}
-
-fn run_lens(num_digits: usize) -> Vec<usize> {
-    (1..=(num_digits / 2)).filter(|x| num_digits % x == 0).collect()
 }
 
 fn num_digits(mut v: usize) -> usize {
@@ -97,24 +80,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_slices_of() {
-        assert_eq!(slices_of(10, 1), vec![1, 0], "(10, 1)");
-        assert_eq!(slices_of(11, 1), vec![1, 1], "(11, 1");
-        assert_eq!(slices_of(1024, 2), vec![10, 24], "(1024, 2)");
-        assert_eq!(slices_of(1004, 2), vec![10, 4], "(1004, 2)");
-    }
+    fn test_repeats_any() {
+        assert_eq!(repeats_any(1), false);
+        assert_eq!(repeats_any(10), false);
+        assert_eq!(repeats_any(101), false);
+        assert_eq!(repeats_any(1010), true);
 
-    #[test]
-    fn test_run_lens() {
-        assert_eq!(run_lens(0), vec![]);
-        assert_eq!(run_lens(1), vec![]);
-        assert_eq!(run_lens(2), vec![1]);
-        assert_eq!(run_lens(3), vec![1]);
-        assert_eq!(run_lens(4), vec![1, 2]);
-        assert_eq!(run_lens(5), vec![1]);
-        assert_eq!(run_lens(6), vec![1, 2, 3]);
-        assert_eq!(run_lens(7), vec![1]);
-        assert_eq!(run_lens(8), vec![1, 2, 4]);
+        assert_eq!(repeats_any(11), true);
+        assert_eq!(repeats_any(111), true);
+        assert_eq!(repeats_any(1111), true);
     }
 
     #[test]
@@ -141,6 +115,6 @@ mod tests {
 
     #[test]
     fn test_pt2() {
-        //assert_eq!(run(IN1, false), 5961);
+        assert_eq!(run(IN1, false), 17298174201);
     }
 }
