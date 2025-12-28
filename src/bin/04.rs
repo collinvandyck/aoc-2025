52eac310dd18e35bc185b845ae223906f52bea45
Collin Van Dyck <collin@5xx.engineer>
2025-12-28 17:37:44 -0500

    Day 04 done.

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -5,13 +5,12 @@ static EX1: &str = include_str!("../../data/04/ex1");
 static IN1: &str = include_str!("../../data/04/in1");
 
 fn main() {
-    println!("ex1: {}", run(EX1, true));
-    println!("in1: {}", run(IN1, true));
+    println!("pt2: {}", run(IN1, false));
 }
 
 fn run(s: &str, pt1: bool) -> usize {
-    let grid = parse(s);
-    grid.max_neighbors(3)
+    let mut grid = parse(s);
+    if pt1 { grid.max_neighbors() } else { grid.max_neighbors_remove() }
 }
 
 #[derive(Clone)]
@@ -27,13 +26,28 @@ struct Tile {
 }
 
 impl Grid {
-    fn iter(&self) -> impl Iterator<Item = &Tile> {
-        self.tiles.iter().flat_map(|row| row.iter())
+    fn max_neighbors_remove(&mut self) -> usize {
+        let mut res = 0;
+        loop {
+            let found: Option<Tile> = self
+                .tiles
+                .iter()
+                .flat_map(|r| r.iter())
+                .filter(|t| t.val == '@' as u8)
+                .find(|t| self.paper_neighbors(t) <= 3)
+                .copied();
+            let Some(found) = found else { break };
+            res += 1;
+            self.tiles[found.row][found.col].val = '.' as u8;
+        }
+        res
     }
-    fn max_neighbors(&self, max: usize) -> usize {
-        self.iter()
+    fn max_neighbors(&self) -> usize {
+        self.tiles
+            .iter()
+            .flat_map(|r| r.iter())
             .filter(|t| t.val == '@' as u8)
-            .filter(|t| self.paper_neighbors(t) <= max)
+            .filter(|t| self.paper_neighbors(t) <= 3)
             .count()
     }
     fn paper_neighbors(&self, tile: &Tile) -> usize {
@@ -41,35 +55,27 @@ impl Grid {
         let rows = self.tiles.len();
         let cols = self.tiles.len();
         let &Tile { row, col, val } = tile;
-        // left
         if col > 0 && self.is_paper(row, col - 1) {
             res += 1;
         }
-        // up
         if row > 0 && self.is_paper(row - 1, col) {
             res += 1;
         }
-        // right
         if col < cols - 1 && self.is_paper(row, col + 1) {
             res += 1;
         }
-        // down
         if row < rows - 1 && self.is_paper(row + 1, col) {
             res += 1;
         }
-        // left up
         if col > 0 && row > 0 && self.is_paper(row - 1, col - 1) {
             res += 1;
         }
-        // left down
         if col > 0 && row < rows - 1 && self.is_paper(row + 1, col - 1) {
             res += 1;
         }
-        // right up
         if col < cols - 1 && row > 0 && self.is_paper(row - 1, col + 1) {
             res += 1;
         }
-        // right down
         if col < cols - 1 && row < rows - 1 && self.is_paper(row + 1, col + 1) {
             res += 1;
         }
@@ -104,5 +110,15 @@ mod tests {
     #[test]
     fn test_pt1_in1() {
         assert_eq!(run(IN1, true), 1464);
+    }
+
+    #[test]
+    fn test_pt2_ex1() {
+        assert_eq!(run(EX1, false), 43);
+    }
+
+    #[test]
+    fn test_pt2_in1() {
+        assert_eq!(run(IN1, false), 8409);
     }
 }
